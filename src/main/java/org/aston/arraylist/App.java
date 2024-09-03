657294c13d8014563927db176ff10a6be498463d
artem <artem.yurev.1997@gmail.com>
2024-09-03 22:55:15 +0300

    Initial commit

---
+++ src/main/java/org/aston/arraylist/App.java
@@ -0,0 +1,14 @@
+package org.aston.arraylist;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/aston/arraylist/ArrayListImpl.java
@@ -0,0 +1,4 @@
+package org.aston.arraylist;
+
+public class ArrayListImpl {
+}
